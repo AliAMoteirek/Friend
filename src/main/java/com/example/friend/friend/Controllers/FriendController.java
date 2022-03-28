a0a86d8b8516e8214abd3a218cf2b78210104177
Ali Moteirek <mtairek20@gmail.com>
2022-03-28 13:50:37 +0200

    fixed some issues

--- src/main/java/com/example/friend/friend/Controllers/FriendController.java
+++ src/main/java/com/example/friend/friend/Controllers/FriendController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping(path="")
+@RequestMapping(path="/friend")
 public class FriendController {
 
     private final FriendService friendService;
@@ -19,34 +19,34 @@ public class FriendController {
         this.friendService = friendService;
     }
 
-    @GetMapping("/")
+    @GetMapping("/friend/")
     public String index() {
         return "Welcome to my friend phone list page";
     }
 
-    @GetMapping("/freind/add")
+    @GetMapping("/add")
     public String addNewFriend(@RequestParam String name,
                                @RequestParam String email,
                                @RequestParam String telephoneNumber){
         return friendService.addNewFriend(name,email,telephoneNumber);
     }
 
-    @RequestMapping("/friend/allfriends")
+    @RequestMapping("/allfriends")
     public Iterable<Friend> getFriends() {
         return friendService.getAllFriends();
     }
 
-    @GetMapping( "/friend/friend")
+    @GetMapping( "/friend")
     public Iterable<Friend> getByName(@RequestParam(required = false) String name){
         return friendService.getAllFriendsByName(name);
     }
 
-    @GetMapping( "/friend/getfriendbynumber")
+    @GetMapping( "/getfriendbynumber")
     public Iterable<Friend> getByNumber(@RequestParam(required = false) String number){
         return friendService.getAllFriendsByNumber(number);
     }
 
-    @GetMapping(value = "/friend/delete/{id}")
+    @GetMapping(value = "/delete/{id}")
     public String deleteFriend(@PathVariable Long id) {
         return friendService.deleteFriend(id);
     }
@@ -58,7 +58,7 @@ public class FriendController {
         friendService.updateStudent(friendId, telephoneNumber);
     }*/
 
-    @PutMapping(path = "/friend/update/{friendId}")
+    @PutMapping(path = "/update/{friendId}")
     public Response updateStudent(@PathVariable Long friendId,
                                   @RequestBody Friend friend){
         return friendService.updateStudent(friendId, friend);
